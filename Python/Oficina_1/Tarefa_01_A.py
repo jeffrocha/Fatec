c087f537bbc85d05607b7b432e50a6f678a7fb65
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-09 20:45:48 -0300

    Tarefa_01_A

---
+++ Python/Oficina_1/Tarefa_01_A.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+from encodings import utf_8
+
+coding: utf_8
+
+os.system('cls||clear')
+
+area = 0
+base = 0
+altura = 0
+
+base = int(input('Entre com o valor da base: '))
+altura = int(input('Entre com o valor da altura: '))
+area = base * altura / 2
+print('A area é:', area)
+time.sleep(5)
+sys.exit
