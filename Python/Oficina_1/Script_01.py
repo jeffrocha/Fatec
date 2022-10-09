a5ee787831ea7a6ff50320e35fa895ede0229933
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-09 18:00:09 -0300

    Script_03

--- Python/Oficina_1/Script_01.py
+++ Python/Oficina_1/Script_01.py
@@ -7,9 +7,10 @@ coding: utf_8  # type: ignore
 area = 0  # variáveis
 raio = 0
 pi = 3.14
+os.system('cls||clear')  # apaga a tela
 raio = input('Digite o valor do raio:')  # ler raio
 area = (float(raio) ** 2) * pi  # calcula a area
-os.system("clear")  # apaga a tela
+
 print('\n')  # pula uma linha
 print(f'A area será:{area:.3f}')  # exibe area
 time.sleep(5)  # pausa de 5 segundos
