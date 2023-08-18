451d1ba78e02a4696a3422f0b7113ee605e973cc
Jefferson R Silva <49925721+jeffrocha@users.noreply.github.com>
2023-08-17 23:55:36 -0300

    Create TAREFA_AVALIAÇÃO_CONTINUADA_1_K.cpp

---
+++ Estrutura_de_Dados/Aula1/TAREFA_AVALIAÇÃO_CONTINUADA_1_K.cpp
@@ -0,0 +1,5 @@
+/*
+K - Elaborar um programa que efetue a apresentação do valor da conversão em dólar de um valor lido em
+real. O programa deve solicitar o valor da cotação do dólar e também a quantidade de reais disponível
+com o usuário, para que seja apresentado o valor em moeda americana
+*/
