2ff75ed52b7b17097556212c63c5acaf7ffa3fd2
Jefferson R Silva <49925721+jeffrocha@users.noreply.github.com>
2023-09-06 10:00:44 -0300

    Create Atividade_B.cpp

---
+++ Estrutura_de_Dados/Aula 4/Atividade_B.cpp
@@ -0,0 +1,17 @@
+/***************************************
+Faça o código fonte dos programas A, B da página 26 do livro Estudo dirigido de 
+Algoritmos. 
+a) Os dados de entrada e saída deverão ser armazenados em um struct com 
+várias colunas; 
+b) Deverá conter menu com switch case, funções para leitura dos dados 
+de entrada e as respectivas funções para calcular os dados de saída; 
+c) Tanto os dados 
+de entrada quando os dados de saída deverão ser armazenados dentro de structs.
+
+1) Desenvolva os algoritmos
+a) Elaborar um programa de computador que efetue a leitura de quatro valores inteiros (variáveis A, B, C e D).
+Ao final o programa deve apresentar o resultado do produto (variável P) do primeiro com o terceiro 
+valor, e o resultado do produto (variável P) do primeiro com o terceiro valor, e o resultado da soma 
+(variável S) do segundo com o quarto valor.
+
+************************************************/
